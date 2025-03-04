84cfcc1848254805f95b00165a3fd8cfbb24eccc
Tonatyw0608 <13020274688@163.com>
2025-03-04 19:58:45 +0800

    添加 API 端点

--- learning/urls.py
+++ learning/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('notes', views.get_notes, name='notes'),
+    path('history', views.get_history, name='history'),
 ]
